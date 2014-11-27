5912a9736f375748e9b551465333ea20cd883ffd
Lena <lzheng22@mail.ccsf.edu>
2014-11-26 21:03:28 -0800

    added more comments and renamed functions

--- vacation.rb
+++ vacation.rb
@@ -2,6 +2,54 @@
 #this program checks to see how much vacation time an employee has accrued
 require 'date'
 
+#each new employee gets their own data set
+class Employee
+	attr_accessor :date_of_hire, :start_date, :end_date, :used_hours, :extra_hours
+
+	#finds how many scheduled working days from a start_date to an end_date
+	def total_workdays
+		n = 1
+		workdays = [1,2,3,4,5] 	#this assumes employees work monday - friday
+		start = Date.parse(@start_date)
+
+		until start == @end_date
+			if workdays.include?(start.wday)
+				n +=1
+			end
+			start = start + 1
+		end
+		n
+	end
+
+	#finds how many hours employee accrued from the start_date to end_date, depending on scheduled working days worked
+	def accrued_hours
+
+	#hours accrued per day depending on years worked
+		less_than_5 = 120.0 / 52 / 5
+		more_than_5 = 160.0 / 52 / 5
+
+		start = Date.parse(@start_date)
+		if Date.parse(date_of_hire).year - start.year < 5
+			total_workdays * less_than_5 - @used_hours + @extra_hours
+
+    	elsif Date.parse(date_of_hire).year - start.year > 5
+    		total_workdays * more_than_5 - @used_hours + @extra_hours
+		end
+	end
+
+end
+
+#test this out
+Lena = Employee.new
+Lena.date_of_hire = '14-10-08'
+Lena.start_date = '2014-10-08'
+Lena.end_date = Date.today
+Lena.used_hours = 0
+Lena.extra_hours = 8
+
+puts Lena.accrued_hours
+
+
 
 ##ignore this for now--
 class Time
@@ -13,62 +61,3 @@ class Time
      [0,6,7].include?(wday)
   end
 end
-
-
-
-#read from a file or something for their hire date
-hire_year = 2014
-hire_month = 10
-hire_day = 8
-hire_date = '2014-10-08'
-
-start_date=Date.parse(hire_date)
-curr_date=Date.today
-
-
-#finds the working day until the given date
-
-class Employee
-	attr_accessor :date_of_hire, :start_date, :end_date, :used_hours, :extra_hours
-
-
-def workdays_until
-	n = 1
-	workdays = [1,2,3,4,5]
-	start = Date.parse(@start_date)
-
-	until start == @end_date
-	if workdays.include?(start.wday)
-	n += 1
-	end
-	start = start + 1
-	end
-	n
-end
-
-
-
-def accrued_hours
-	#hours accrued per day
-	less_than_5 = 120.0 / 52 / 5
-	more_than_5 = 160.0 / 52 / 5
-	
-	start = Date.parse(@start_date)
-	if Date.parse(date_of_hire).year - start.year < 5
-	workdays_until * less_than_5 - @used_hours + @extra_hours
-
-    elsif Date.parse(date_of_hire).year - start.year > 5
-    workdays_until * more_than_5 - @used_hours + @extra_hours
-	end
-end
-
-end
-
-Lena = Employee.new
-Lena.date_of_hire = '14-10-08'
-Lena.start_date = '2014-10-08'
-Lena.end_date = Date.today
-Lena.used_hours = 0
-Lena.extra_hours = 8 #flu shot
-
-puts Lena.accrued_hours
